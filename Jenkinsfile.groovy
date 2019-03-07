bc1eb3f9d451c345ce6ef6025ec7e498cfcb2009
BIBOBIBO <fxw1988@126.com>
2019-03-07 15:10:10 +0800

    增加测试

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,9 +7,27 @@ pipeline {
 			}
 		}
 
+		stage("静态代码扫描") {
+			steps {
+				sh "npm run lint"
+			}
+		}
+
 		stage("跑单元测试") {
 			steps {
 				sh "npm run test:unit"
+			}
+		}
+
+		stage("构建打包") {
+			steps {
+				sh "npm run build"
+			}
+		}
+
+		stage("部署上线") {
+			steps {
+				echo "部署上线"
 			}
 		}
 	}
